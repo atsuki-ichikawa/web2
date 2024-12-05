f1198f6118f848128790a39eafb8e0cae7167ae0
ucbssnj <k22010kk@aitech.ac.jp>
2024-12-05 10:06:06 +0900

    directory変更

--- ex-gen-app1/routes/hello.js
+++ ex-gen-app1/routes/hello.js
@@ -2,18 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+	var name = req.query.name;
+	var mail = req.query.mail;
 	var data = {
 		title: 'Hello!',
-		content: '※何か書いて送信して下さい。'
-	};
-	res.render('hello', data);
-});
-
-router.post('/post', (req, res, next) => {
-	var msg = req.body['message'];
-	var data = {
-		title: 'Hello!',
-		content: 'あなたは「' + msg + '」と送信しました。'
+		content: 'あなたの名前は、' + name + '。<br>' +
+			'メールアドレスは、' + mail + 'です。'
 	};
 	res.render('hello', data);
 });
